04f835f1c8b315ca3533f086303cfe46be3c2622
Nathan Tebbs <nathan.tebbs2004@gmail.com>
2024-07-12 02:38:18 -0700

    landing page & github link

--- src/app/home/_components/leftnav.tsx
+++ src/app/home/_components/leftnav.tsx
@@ -1,15 +1,16 @@
 import Image from "next/image";
 export default function LeftNav() {
 	return (
-		<div className="flex w-full h-full flex-col p-5 overflow-hidden items-center md:self-start">
+		<div className="flex min-h-screen w-full h-full flex-col p-5 overflow-hidden items-center md:self-start">
 			<Image
+				priority={true}
 				src="https://utfs.io/f/8dab068b-fd6f-45a7-96c6-4dac9e99bb64-75ryzc.png"
 				width={256}
 				height={128}
 				alt="dev log logo"
 			/>
 			<div className="grid grid-rows-3 p-5 gap-4 min-h-96 h-full w-full">
-				<div className="p-5 border-2 border-border h-full">Preview</div>
+				<div className="p-5 border-2 border-border h-full"></div>
 				<div className="p-5 border-2 border-border h-full"></div>
 				<div className="p-5 border-2 border-border h-full"></div>
 			</div>
